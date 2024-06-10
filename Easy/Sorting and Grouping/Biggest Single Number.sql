9d3f4d8543087970bdb8e748e85efe22e2badaa4
Mohd Monish <122079171+MohdMonish24@users.noreply.github.com>
2024-06-10 17:48:23 +0530

    Uplode

---
+++ Easy/Sorting and Grouping/Biggest Single Number.sql
@@ -0,0 +1,75 @@
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 3   |
+| 3   |
+| 1   |
+| 4   |
+| 5   |
+| 6   |
++-----+
+Output: 
++-----+
+| num |
++-----+
+| 6   |
++-----+
+Explanation: The single numbers are 1, 4, 5, and 6.
+Since 6 is the largest single number, we return it.
+Example 2:
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 7   |
+| 7   |
+| 3   |
+| 3   |
+| 3   |
++-----+
+Output: 
++------+
+| num  |
++------+
+| null |
++------+
+Explanation: There are no single numbers in the input table so we return null.
+
+  
+  solution
+SELECT MAX(num) AS num /*Using aggregation function MAX to find max. no. from MyNumbers Table alias as num (show the column)... */
+FROM MyNumbers /*Association from MyNumbers Table... */
+WHERE num IN /*Using sub - query for num column... */
+(SELECT num /* */
+FROM MyNumbers /* */
+GROUP BY num /*in sub - query we grouped w.r.t. num column... */
+HAVING COUNT(*) = 1); /*Condition given in the problem...(Number should be single...) */
