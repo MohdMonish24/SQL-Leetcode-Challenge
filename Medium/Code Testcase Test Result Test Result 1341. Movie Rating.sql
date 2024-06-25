954194ab040c842f62b790f3c5fb9118b96b7f57
Mohd Monish <122079171+MohdMonish24@users.noreply.github.com>
2024-06-26 04:34:46 +0530

    Create  Code Testcase Test Result Test Result 1341. Movie Rating.sql

---
+++ Medium/Code Testcase Test Result Test Result 1341. Movie Rating.sql
@@ -0,0 +1,110 @@
+Table: Movies
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| title         | varchar |
++---------------+---------+
+movie_id is the primary key (column with unique values) for this table.
+title is the name of the movie.
+ 
+
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+ 
+
+Table: MovieRating
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| user_id       | int     |
+| rating        | int     |
+| created_at    | date    |
++---------------+---------+
+(movie_id, user_id) is the primary key (column with unique values) for this table.
+This table contains the rating of a movie by a user in their review.
+created_at is the user's review date. 
+ 
+
+Write a solution to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+MovieRating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+Explanation: 
+Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+
+
+Approach and Explanation:
+• To solve the problem, we need to perform two separate queries and then combine their results using UNION ALL.
+• The first query finds the user who has rated the greatest number of movies. We achieve this by joining the MovieRating and Users tables on user_id, grouping by name, and ordering the results by the count of ratings in descending order and by name lexicographically in case of a tie.
+• The second query identifies the movie with the highest average rating in February 2020. This is done by joining the MovieRating and Movies tables on movie_id, filtering the ratings to only those within February 2020, grouping by title, and ordering by the average rating in descending order and by title lexicographically in case of a tie.
+• Finally, we use UNION ALL to combine the results of these two queries into a single result set.
+
+
+# Write your MySQL query statement below
+(SELECT name AS results FROM MovieRating JOIN Users USING (user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1)
+UNION ALL
+(SELECT title AS results
+FROM MovieRating JOIN Movies USING(movie_id) WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29' 
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1);
